e55fb8bed0eb832b846c01983e1c9802b2f37596
Humayun Kabir <pythonloverkabir11@gmail.com>
2024-02-02 14:45:55 +0600

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -25,39 +25,57 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  openPhone(String phone) async {
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: const Center(
-        child: Column(
-          children: [],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:()=> openPhone(),
-        child: const Icon(Icons.call),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-  openPhone() async {
-    String phone =  "01913682281";
-    Uri phoneno = Uri.parse('tel:$phone');
-    if(await canLaunchUrl(phoneno)){
-      await launchUrl(phoneno);
+    Uri phoneNo = Uri.parse('tel:$phone');
+    if(await canLaunchUrl(phoneNo)){
+      await launchUrl(phoneNo);
     }else {
       //cannot launch phone
     }
   }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              String phone =  "+88 01913682281";
+              openPhone(phone);
+            },
+                child: const Icon(Icons.call_outlined),
+
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+
+                textStyle: TextStyle(color: Colors.white)
+
+              ),)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+
+        onPressed:(){
+          String phone =  "+88 01913682281";
+          openPhone(phone);
+        },
+
+
+        child: const Icon(Icons.call),
+
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
 
 }
